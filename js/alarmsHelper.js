9d63879275f5c464c2a125f2a03c7bed935ca2f3
Germán Toro del Valle <gtorodelvalle@gmail.com>
2013-10-18 17:37:53 +0200

    Show the list of alarms set.

--- js/alarmsHelper.js
+++ js/alarmsHelper.js
@@ -2,59 +2,60 @@
 
 var AlarmsHelper = (function() {
 
-	function ring() {
-		navigator.vibrate(2000);
-		alert('RING!!!');
-	}
+        function ring() {
+                updateAlarmList();
+                navigator.vibrate(2000);
+                alert('RING!!!');
+        }
 
-	if (navigator.mozAlarms) {
-	  navigator.mozHasPendingMessage('alarm');
-	  navigator.mozSetMessageHandler('alarm', function (mozAlarm) {
-		  var req = navigator.mozApps.getSelf();
-			req.onsuccess = function() {
-				req.result.launch();
-				setTimeout(ring);
-			};
-		});
-	} else {
-		// How can I know whether my app is launched from Alarm or not?
-	}
+        if (navigator.mozAlarms) {
+          navigator.mozHasPendingMessage('alarm');
+          navigator.mozSetMessageHandler('alarm', function (mozAlarm) {
+                  var req = navigator.mozApps.getSelf();
+                        req.onsuccess = function() {
+                                req.result.launch();
+                                setTimeout(ring);
+                        };
+                });
+        } else {
+                // How can I know whether my app is launched from Alarm or not?
+        }
 
-	var parseTime = function(time) {
-	  var parsed = time.split(':');
-	  var hour = +parsed[0]; // cast hour to int, but not minute yet
-	  var minute = parsed[1];
+        var parseTime = function(time) {
+          var parsed = time.split(':');
+          var hour = +parsed[0]; // cast hour to int, but not minute yet
+          var minute = parsed[1];
 
-	  // account for 'AM' or 'PM' vs 24 hour clock
-	  var periodIndex = minute.indexOf('M') - 1;
-	  if (periodIndex >= 0) {
-		  hour = (hour == 12) ? 0 : hour;
-		  hour += (minute.slice(periodIndex) == 'PM') ? 12 : 0;
-		  minute = minute.slice(0, periodIndex);
-	  }
+          // account for 'AM' or 'PM' vs 24 hour clock
+          var periodIndex = minute.indexOf('M') - 1;
+          if (periodIndex >= 0) {
+                  hour = (hour == 12) ? 0 : hour;
+                  hour += (minute.slice(periodIndex) == 'PM') ? 12 : 0;
+                  minute = minute.slice(0, periodIndex);
+          }
 
-	  return {
-		  hour: hour,
-		  minute: +minute // now cast minute to int
-	  };
-	};
+          return {
+                  hour: hour,
+                  minute: +minute // now cast minute to int
+          };
+        };
 
   function addAlarm(time, callback) {
-		time = parseTime(time);
-		var date = new Date();
-		date.setHours(time.hour, time.minute, 0, 0);
-		var request = navigator.alarms.add(date, 'ignoreTimezone');
+                time = parseTime(time);
+                var date = new Date();
+                date.setHours(time.hour, time.minute, 0, 0);
+                var request = navigator.alarms.add(date, 'ignoreTimezone');
 
-		request.onsuccess = function () {
-			console.log("The alarm has been scheduled");
-			callback();
-		};
+                request.onsuccess = function () {
+                        console.log("The alarm has been scheduled");
+                        callback();
+                };
 
-		request.onerror = function () {
-			console.error("An error occurred: " + this.error.name);
-			callback();
-		};
-	}
+                request.onerror = function () {
+                        console.error("An error occurred: " + this.error.name);
+                        callback();
+                };
+        }
 
   return {
     addAlarm: addAlarm
--- js/app.js
+++ js/app.js
@@ -19,8 +19,80 @@ document.getElementById('alarm-new-details-back').addEventListener('click', func
 
 document.getElementById('alarm-new-details-done').addEventListener('click', function (ev) {
   ev.preventDefault();
+  document.getElementById('show-alarms').removeAttribute('disabled');
   AlarmsHelper.addAlarm(document.getElementById('time').value,
                         utils.navigation.back);
+});
+
+function resetAlarmList() {
+  var alarmList = document.querySelector('#alarm-list ul');
+  alarmList.innerHTML = '';
+}
+
+function addAlarmItem(alarm) {
+  var alarmList = document.querySelector('#alarm-list ul');
+  var alarmLI = document.createElement('li');
+  var alarmP = document.createElement('p');
+  var alarmLabel = document.createElement('label');
+  alarmLabel.classList.add('pack-checkbox');
+  var alarmInput = document.createElement('input');
+  alarmInput.setAttribute('type', 'checkbox');
+  alarmInput.setAttribute('checked', true);
+  var alarmSpan = document.createElement('span');
+  alarmLabel.appendChild(alarmInput);
+  alarmLabel.appendChild(alarmSpan);
+  alarmP.appendChild(alarmLabel);
+  var alarmStr;
+  if (alarm) {
+    var alarmDate = new Date(alarm.date);
+    alarmStr = (alarm.data ? alarm.data + ': ' : '') +
+      alarmDate.getDate() + '/' + (alarmDate.getMonth() + 1) + '/' +
+      alarmDate.getFullYear() + ' ' +
+      (alarmDate.getHours() < 10 ? '0' + alarmDate.getHours() :
+        alarmDate.getHours()) + ':' +
+      (alarmDate.getMinutes() < 10 ? '0' + alarmDate.getMinutes() :
+       alarmDate.getMinutes());
+  } else {
+    alarmLabel.classList.add('hidden');
+    alarmStr = 'NO ALARMS';
+  }
+  var alarmText = document.createTextNode(alarmStr);
+  alarmP.appendChild(alarmText);
+  alarmLI.appendChild(alarmP);
+  alarmList.appendChild(alarmLI);
+}
+
+function updateAlarmList() {
+  var alarmsRequest = navigator.alarms.getAll();
+  alarmsRequest.onsuccess = function (ev) {
+    resetAlarmList();
+    if (this.result.length) {
+      document.getElementById('show-alarms').removeAttribute('disabled');
+      var i, alarms = this.result;
+      for (i = 0; i < alarms.length; i++) {
+        addAlarmItem(alarms[i]);
+      }
+    } else {
+      document.getElementById('show-alarms').setAttribute('disabled', true);
+      addAlarmItem();
+    }
+  };
+  alarmsRequest.onerror = function (error) {
+    console.log("ERROR");
+  };
+}
+
+document.getElementById('show-alarms').addEventListener('click', function (ev) {
+  updateAlarmList();
+  utils.navigation.go('#alarm-list');
+});
+
+document.getElementById('alarm-list-back').addEventListener('click', function (ev) {
+  utils.navigation.back();
+});
+
+document.getElementById('alarm-list-done').addEventListener('click', function (ev) {
+  utils.navigation.back();
 });
 
 var stopWatchStart = document.getElementById('stopwatch-start');
